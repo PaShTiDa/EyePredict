3e4b7171805c4b4765d2fca1c548f1c8fc98aa5b
Roee Malis <roeemalis@gmail.com>
2017-06-11 16:55:08 +0300

    Initial Version of the datamanager script

--- project/datamodule/datamanager.py
+++ project/datamodule/datamanager.py
@@ -1,62 +1,103 @@
+import os
+
+subjects = []
+stimuli = []
+screenResolution = None
+stimuliPath = ""
+eyeTrackerDataPath = ""
+eyeTrackerData = set()
+behavioralData = set()
+behavioralDataPath = ""
 
 
-class DataManager:
-    _mainDirectoryPath = ""
-
-    def __init__(self, gui_logic):
-        self.gui_logic = gui_logic()
-
-
-    def VerifyPath(self):
-        """
-        verifys the path is valid, and contains 3 directories: 
-        Eye-tracking data, Behavioral data, Images
-        :return: bool True/False
-        """
-        raise NotImplementedError
-
-    def VerifyData(self):
-        """
-        checks the folders content.
-        expects the same number of edf and txt files,
-        with appropriately fitting file names for each couple.
-        (do we need to test the files aren't empty?)
-        :return: bool True/False
-        """
-        raise NotImplementedError
-
-    def BrowseDirectory(self, path):
-        """
-        Button "Browse" gets a directory path, and sends it to this function.
-        """
-        rc = True
-        _mainDirectoryPath = path
-
-        # Verify Data
-        if(!VerifyPath(self)):
-            error = "Bad data path.\n Eye-Predict expects the main folder to contain 3 subfolders:\n Eye-tracking data, Behavioral data, Images"
-            rc = False
-        if(rc && !VerifyData(self)):
-            error = "..."
-        if(!rc):
-            Error(self, error)
-            return -1
-
-        # Load Data
-        #miew
+def verifyPath(taskPropertiesPath):
+    """
+    verifys the path is valid, and contains 3 directories:
+    Eye-tracking data, Behavioral data, Images
+    :return: bool True/False
+    """
+    global screenResolution, eyeTrackerDataPath, behavioralDataPath, stimuliPath
+    with open(taskPropertiesPath, 'r') as file:
+        for line in file:
+            if line.startswith("#"):
+                continue
+            split = line.split()
+            if len(split) >= 2:
+                if split[0] == "ScreenResolution":
+                    if len(split) == 3:
+                        screenResolution = (split[1], split[2])
+                    else:
+                        return False
+                elif split[0] == "Path2Stimuli" and os.path.isdir(split[1]):
+                    stimuliPath = split[1]
+                elif split[0] == "Path2EDF" and os.path.isdir(split[1]):
+                    eyeTrackerDataPath = split[1]
+                elif split[0] == "Path2Datafile" and os.path.isdir(split[1]):
+                    behavioralDataPath = split[1]
+                elif split[0] == "StimIdentifier":
+                    stimulus = split[1]
+                    split = file.readline().split()
+                    if len(split) != 5:
+                        return False
+                    stimLocation = (split[1], split[2], split[3], split[4])
+                    stimuli.append((stimulus, stimLocation))
+                else:
+                    return False
+            else:
+                return False
+    return True
 
 
-        raise NotImplementedError
+def verifyData():
+    """
+    checks the folders content.
+    expects the same number of edf and txt files,
+    with appropriately fitting file names for each couple.
+    (do we need to test the files aren't empty?)
+    :return: bool True/False
+    """
+    global eyeTrackerData, behavioralData
+    ascFiles = [f[:-4] for f in os.listdir(eyeTrackerDataPath) if os.path.isfile(os.path.join(eyeTrackerDataPath, f)) and f.endswith(".asc")]
+    eyeTrackerData = set(ascFiles)
+    txtFiles = [f[:-4] for f in os.listdir(eyeTrackerDataPath) if os.path.isfile(os.path.join(eyeTrackerDataPath, f)) and f.endswith(".txt")]
+    print(txtFiles)
+    print(ascFiles)
+    behavioralData = set(txtFiles)
+    if eyeTrackerData == behavioralData:
+        return True
+    else:
+        return False
 
-    def Error(self, errorString):
-        """
-        pops a gui error dialog box
-        :param errorString: the error to prompt
-        :return: non
-        """
-        raise NotImplementedError
+def BrowseDirectory(path):
+    """
+    Button "Browse" gets a directory path, and sends it to this function.
+    """
+    rc = True
+
+    # Verify Data
+    error = ""
+    if(not verifyPath(path)):
+        error = "Bad data path.\n Eye-Predict expects the main folder to contain 3 subfolders:\n Eye-tracking data, Behavioral data, Images"
+        rc = False
+    if(not verifyData()):
+        error = "..."
+    if(not rc):
+        Error(error)
+        return -1
+
+    # Load Data
+
+    raise NotImplementedError
+
+def Error(errorString):
+    """
+    pops a gui error dialog box
+    :param errorString: the error to prompt
+    :return: non
+    """
 
 
-
+print(verifyPath("C:\\Users\\Roee\\Desktop\\edf\\task.tsk"))
+print(verifyData())
 
 
