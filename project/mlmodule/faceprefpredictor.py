88b850f4e4a6c177a4a6a1c73a7687def4dafc8b
Roee Malis <roeemalis@gmail.com>
2017-06-11 18:26:52 +0300

    face predictor small update

--- project/mlmodule/faceprefpredictor.py
+++ project/mlmodule/faceprefpredictor.py
@@ -5,14 +5,22 @@ class FacePrefPredictor(IPredict):
 
     def __init__(self):
         self.classifier = svm.SVC()
+        self.isTrained = False
 
     def changeClassifier(self, newClassifier):
         self.classifier = newClassifier
+        self.isTrained = False
 
     def train_classifier(self, X, y):
         self.classifier.fit(X, y)
+        self.isTrained = True
 
     def get_prediction_accuracy(self):
-        #TODO: Use the Kfold to implement a 1 subject out or 1 trial out strategies.
-        pass
+        if self.isTrained:
+            #get accuracy
+            #TODO: use kfold to implement 1 subject out or 1 trial out.
+            pass
+        else:
+            #must train first
+            pass
 
